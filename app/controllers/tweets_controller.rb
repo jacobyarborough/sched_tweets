7758bf26bede3da0dcca662fe4bec0fe337db7f9
Jacob Yarborough <jayarborough95@gmail.com>
2024-02-05 08:14:10 -0500

    Create CRUD funcationaility for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,21 +2,55 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    current_user.twitter_accounts.find(params[:twitter_account_id]).tweets
+    twitter_account = current_user.twitter_accounts.find(params[:twitter_account_id])
+    TwitterFacade.get_tweets(current_user, twitter_account)
+    # current_user.twitter_accounts.find(params[:twitter_account_id]).tweets
+  end
+
+  def my_tweets
+    @tweets = current_user.twitter_accounts.find(params[:twitter_account_id]).tweets
   end
 
   def new
+    @tweet = current_user.twitter_accounts.find(params[:twitter_account_id]).tweets.new()
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update(tweet_params)
+      flash[:notice] = "Tweet has been successfully updated"
+      redirect_to twitter_account_my_tweets_path(current_user.twitter_accounts.find(params[:twitter_account_id]))
+    else
+      render :edit, status: :unprocessable_entity
+    end 
   end
 
   def create
+    @tweet = current_user.tweets.new(tweet_params)
+    @tweet.twitter_account_id = params[:twitter_account_id]
+
+    if @tweet.save
+      flash[:notice] = "successfully scheduled the tweet!"
+      redirect_to twitter_account_my_tweets_path(current_user.twitter_accounts.find(params[:twitter_account_id]))
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    Tweet.find(params[:id]).destroy
+    flash[:notice] = "Successfully deleted tweet"
+    redirect_to twitter_account_my_tweets_path(current_user.twitter_accounts.find(params[:twitter_account_id]))
   end
 
   private
 
-  def tweets_params
-    params.require(:tweet).permit()
+  def tweet_params
+    params.require(:tweet).permit(:body, :publish_at)
   end
 end
